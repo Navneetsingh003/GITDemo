5b2e3c6c0002d062f629bef953ea3bd31d6ab4f6
Navneetsingh003 <navneet.singh.3003@gmail.com>
2020-03-04 22:02:22 +0530

    This is commit after first update.

--- src/test/java/com/mycompany/app/SeleniumTest.java
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -8,6 +8,9 @@ import org.testng.annotations.Test;
 		public void seleniumTestCode() {
 		
 			System.out.println("seleniumTestCode");
+			System.out.println("This is update in local repository.");
+			System.out.println("After update, need to push the updated code to GitHub");
+			
 		}
 
 }
