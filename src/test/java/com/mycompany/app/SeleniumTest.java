fa62918f15d2178b61264143f43febff6440b34b
Navneetsingh003 <navneet.singh.3003@gmail.com>
2020-03-06 10:01:45 +0530

    First update in child branch i.e. develop

--- src/test/java/com/mycompany/app/SeleniumTest.java
+++ src/test/java/com/mycompany/app/SeleniumTest.java
@@ -11,6 +11,10 @@ import org.testng.annotations.Test;
 			System.out.println("This is update in local repository.");
 			System.out.println("After update, need to push the updated code to GitHub");
 			
+			System.out.println("A new branch - develop is created.");
+			System.out.println("Changes are done in develop branch.");
+			System.out.println("Master branch is not impacted because of these changes.");
+			
 		}
 
 }
